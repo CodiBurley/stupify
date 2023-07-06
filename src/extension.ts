7f6440e7a8769c28bfea238a398803d031c61d32
Codi Burley <codi.burley@gmail.com>
2023-07-06 11:47:28 -0400

    chore: change name of refactor command

--- src/extension.ts
+++ src/extension.ts
@@ -111,15 +111,11 @@ async function getRefactorAICompletionResponseStream(
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "stupify" is now active!');
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "stupify.helloWorld",
+    "stupify.refactorViewModel",
     async () => {
       const currentFileContent = getValidatedTextFromActiveEditor();
       if (!currentFileContent) {
