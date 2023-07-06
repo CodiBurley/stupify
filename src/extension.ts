543715c53431bbc77000b99e2e092a2754a11cb5
Codi Burley <codi.burley@gmail.com>
2023-07-05 23:29:14 -0400

    Read contents of current file and open new file to append refactored result

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,20 @@ function initializeOpenAI() {
   return new OpenAIApi(configuration);
 }
 
+async function openNewTextDocumentWithSameLanguage() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showInformationMessage(
+      "Please open a file and show me some code"
+    );
+    return;
+  }
+  const document = editor.document;
+  const language = document.languageId;
+  const doc = await vscode.workspace.openTextDocument({ language });
+  vscode.window.showTextDocument(doc, editor.viewColumn);
+}
+
 let lastEdit = Promise.resolve(true);
 
 async function appendTextToActiveEditor(text: string) {
@@ -30,6 +44,71 @@ async function appendTextToActiveEditor(text: string) {
   }
 }
 
+async function appendResponseStreamToNewFile(
+  response: Awaited<ReturnType<typeof OpenAIApi.prototype.createChatCompletion>>
+) {
+  await openNewTextDocumentWithSameLanguage();
+  for await (const data of response.data as any) {
+    const lines = data
+      .toString("utf-8")
+      .split("\n")
+      .filter((line: string) => line.trim() !== "");
+    for (const line of lines) {
+      const message = line.replace(/^data: /, "");
+      try {
+        const parsed = JSON.parse(message);
+        if (Boolean(parsed.choices[0]?.finish_reason)) {
+          // finish logic here
+          continue;
+        }
+        const content = parsed.choices[0]?.delta?.content;
+        if (!content) {
+          continue;
+        }
+        await appendTextToActiveEditor(content);
+      } catch (error) {
+        console.error("Could not JSON parse stream message", message, error);
+      }
+    }
+  }
+}
+
+function getValidatedTextFromActiveEditor() {
+  const MINIMUM_LENGTH = 120;
+
+  const currentFileContent = vscode.window.activeTextEditor?.document.getText();
+
+  if (!currentFileContent || currentFileContent.length < MINIMUM_LENGTH) {
+    vscode.window.showInformationMessage(
+      "Please provide more code to work with"
+    );
+    return;
+  }
+
+  return currentFileContent;
+}
+
+async function getRefactorAICompletionResponseStream(
+  currentFileContent: string
+) {
+  const openai = initializeOpenAI();
+  return openai.createChatCompletion(
+    {
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [
+        { role: "system", content: systemPrompt },
+        {
+          role: "user",
+          content: currentFileContent,
+        },
+      ],
+      temperature: 0,
+    },
+    { responseType: "stream" }
+  );
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
@@ -43,46 +122,13 @@ export async function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "stupify.helloWorld",
     async () => {
-      const openai = initializeOpenAI();
-      const response = await openai.createChatCompletion(
-        {
-          model: "gpt-3.5-turbo",
-          stream: true,
-          messages: [
-            { role: "system", content: systemPrompt },
-            { role: "user", content: userPrompt },
-          ],
-        },
-        { responseType: "stream" }
-      );
-      for await (const data of response.data as any) {
-        const lines = data
-          .toString("utf-8")
-          .split("\n")
-          .filter((line: string) => line.trim() !== "");
-        for (const line of lines) {
-          const message = line.replace(/^data: /, "");
-          try {
-            const parsed = JSON.parse(message);
-            if (Boolean(parsed.choices[0]?.finish_reason)) {
-              // finish logic here
-              continue;
-            }
-            const content = parsed.choices[0]?.delta?.content;
-            if (!content) {
-              continue;
-            }
-            await appendTextToActiveEditor(content);
-          } catch (error) {
-            console.error(
-              "Could not JSON parse stream message",
-              message,
-              error
-            );
-          }
-        }
+      const currentFileContent = getValidatedTextFromActiveEditor();
+      if (!currentFileContent) {
+        return;
       }
-      vscode.window.showInformationMessage(JSON.stringify(response));
+      const refactorResponseStream =
+        await getRefactorAICompletionResponseStream(currentFileContent);
+      await appendResponseStreamToNewFile(refactorResponseStream);
     }
   );
 
